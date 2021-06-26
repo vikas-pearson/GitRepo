de7911c4d0676e980208b66a7c598f880b8a94fd
Vikas <vikasmittal1985@gmail.com>
2021-06-26 15:45:22 +0530

    Added two more lines

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -16,6 +16,9 @@ public class HelloWorld {
         System.out.println("Hello World3");
         System.out.println("Hello World4");
         System.out.println("Hello World5");
+        System.out.println("Hello World6");
+        System.out.println("Hello World7");
+
         System.setProperty("webdriver.chrome.driver", "C:\\Data\\DriveA\\Java exe(New Laptop)\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
